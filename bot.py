e25f35a4d2c4484fd2fa073f6abf4727c4928d47
Janamaus <jana.amfalder@gmail.com>
2025-01-15 03:21:45 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
 from flask import Flask, request
 import telegram
 
-# Flask-Server erstellen
 app = Flask(__name__)
 
-# Telegram-Bot initialisieren
 BOT_TOKEN = "7733972368:AAFl4oyP5S6Zea13GePBgG0ZLwv539qU0kA"
 bot = telegram.Bot(token=BOT_TOKEN)
 
@@ -13,8 +11,6 @@ def webhook():
     update = telegram.Update.de_json(request.get_json(force=True), bot)
     chat_id = update.message.chat.id
     message = update.message.text
-
-    # Antwort senden
     bot.sendMessage(chat_id=chat_id, text=f"Du hast gesagt: {message}")
     return "ok"
 
